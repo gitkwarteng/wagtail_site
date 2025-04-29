b2239d5e9619b77cd1acd10eed48ba3282542f6d
Antwi Kwarteng-Amaning <mailantwi@icloud.com>
2025-04-29 18:09:38 +0000

    added method for getting page site template.

--- wagtail_site/models.py
+++ wagtail_site/models.py
@@ -31,7 +31,7 @@ class AbstractWebPage(DefaultTemplatesMixin, models.Model):
 
     base_template_name = 'wagtail_site/layout/base.html'
 
-    template = get_template_for_web_page()
+    web_template = None
 
     banner = models.ForeignKey('wagtail_site.WebPageBanner', null=True, blank=True, verbose_name="Banner", on_delete=models.SET_NULL,
                                related_name='+')
@@ -63,9 +63,9 @@ class AbstractWebPage(DefaultTemplatesMixin, models.Model):
 
     def get_template(self, request, *args, **kwargs):
         if request.headers.get("x-requested-with") == "XMLHttpRequest":
-            return self.ajax_template or self.template or self.page_template
+            return self.ajax_template or self.web_template or self.page_template
         else:
-            return self.template or self.page_template
+            return self.web_template or self.page_template
 
 
 
