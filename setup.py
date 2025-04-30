51b2fb239537bb9293be7cebcfa8c771b8daa856
Antwi Kwarteng-Amaning <mailantwi@icloud.com>
2025-04-30 03:49:12 +0000

    Release: v0.2.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtail_site',
-    version='0.2.9',
+    version='0.2.10',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
